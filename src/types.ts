1d020a6ca539596024b16682cf9f800606ddc649
anselbrandt <ansel@anselbrandt.com>
2020-11-14 22:09:21 -0500

    Add favoriteLoader type

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import { Redis } from "ioredis";
+import { createFavoriteLoader } from "./utils/createFavoriteLoader";
 
 export type MyContext = {
   req: Request & { session: Express.Session };
   redis: Redis;
   res: Response;
+  favoriteLoader: ReturnType<typeof createFavoriteLoader>;
 };
