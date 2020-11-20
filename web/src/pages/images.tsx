78a98d05898f615f141156cc2dd7f7647f007235
anselbrandt <ansel@anselbrandt.com>
2020-11-20 02:28:25 -0500

    List images subs by category

--- web/src/pages/images.tsx
+++ web/src/pages/images.tsx
@@ -1,8 +1,9 @@
-import { Box, Link as ChakraLink } from "@chakra-ui/core";
+import { Box, Link as ChakraLink, Heading } from "@chakra-ui/core";
 import { Container } from "../components/Container";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import { Navbar } from "../components/Navbar";
 import useTokenFetch from "../hooks/useTokenFetch";
+import { allsubs } from "../subs";
 
 interface ImagesProps {
   defaultColor: string;
@@ -19,6 +20,18 @@ const Images: React.FC<ImagesProps> = ({ defaultColor }) => {
       <Box mt="30vh" maxW="48rem">
         <Box>Images</Box>
         <Box>{token}</Box>
+        {allsubs.map((value, index) => (
+          <Box key={index}>
+            <Heading>{value.category}</Heading>
+            <Box>
+              {value.subs.map((value, index) => (
+                <Box key={index}>
+                  <ChakraLink href={`/images/${value}`}>{value}</ChakraLink>
+                </Box>
+              ))}
+            </Box>
+          </Box>
+        ))}
       </Box>
     </Container>
   );
