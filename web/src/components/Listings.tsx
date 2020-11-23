42f846275830c2aeebf4db2eac9456b1eefa7d5d
anselbrandt <ansel@anselbrandt.com>
2020-11-23 01:35:53 -0500

    Add Listings component

---
+++ web/src/components/Listings.tsx
@@ -0,0 +1,66 @@
+import {
+  Grid,
+  Box,
+  Link as ChakraLink,
+  AspectRatio,
+  Image,
+  Flex,
+  Spinner,
+} from "@chakra-ui/core";
+
+interface ListingsProps {
+  fetchedListings: any[];
+  isLoading: boolean;
+  sub: string;
+  setBottom: any;
+}
+
+export const Listings: React.FC<ListingsProps> = ({
+  fetchedListings,
+  setBottom,
+  sub,
+  isLoading,
+}) => {
+  return (
+    <Grid
+      templateColumns="repeat(3, 1fr)"
+      gap={1}
+      width="100%"
+      maxWidth="1000px"
+    >
+      {fetchedListings.map((value, index) => {
+        if (index === fetchedListings.length - 1) {
+          return (
+            <Box key={value.id} ref={setBottom}>
+              <ChakraLink href={`${sub}/${value.id}`}>
+                <AspectRatio ratio={1}>
+                  <Image src={value.preview} objectFit="cover" />
+                </AspectRatio>
+              </ChakraLink>
+            </Box>
+          );
+        } else {
+          return (
+            <Box key={value.id}>
+              <ChakraLink href={`${sub}/${value.id}`}>
+                <AspectRatio ratio={1}>
+                  <Image src={value.preview} objectFit="cover" />
+                </AspectRatio>
+              </ChakraLink>
+            </Box>
+          );
+        }
+      })}
+      {isLoading ? (
+        <Flex
+          width="100%"
+          height="100%"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Spinner thickness="4px" speed="0.65s" size="xl" />
+        </Flex>
+      ) : null}
+    </Grid>
+  );
+};
--- web/src/pages/images/[sub].tsx
+++ web/src/pages/images/[sub].tsx
@@ -1,18 +1,11 @@
-import {
-  AspectRatio,
-  Box,
-  Link as ChakraLink,
-  Image,
-  Grid,
-  Spinner,
-  Flex,
-} from "@chakra-ui/core";
+import { Link as ChakraLink } from "@chakra-ui/core";
 import { useRouter } from "next/router";
 import { useRef, useEffect, useState } from "react";
 import { Container } from "../../components/Container";
 import { DarkModeSwitch } from "../../components/DarkModeSwitch";
 import { Navbar } from "../../components/Navbar";
 import useListingsFetch from "../../hooks/useListingsFetch";
+import { Listings } from "../../components/Listings";
 
 interface SubProps {
   defaultColor: string;
@@ -58,46 +51,12 @@ const Sub: React.FC<SubProps> = ({ defaultColor }) => {
       <Navbar defaultColor={defaultColor}>
         <ChakraLink href="/">Home</ChakraLink>
       </Navbar>
-      <Grid
-        templateColumns="repeat(3, 1fr)"
-        gap={1}
-        width="100%"
-        maxWidth="1000px"
-      >
-        {fetchedListings.map((value, index) => {
-          if (index === fetchedListings.length - 1) {
-            return (
-              <Box key={value.id} ref={setBottom}>
-                <ChakraLink href={`${sub}/${value.id}`}>
-                  <AspectRatio ratio={1}>
-                    <Image src={value.preview} objectFit="cover" />
-                  </AspectRatio>
-                </ChakraLink>
-              </Box>
-            );
-          } else {
-            return (
-              <Box key={value.id}>
-                <ChakraLink href={`${sub}/${value.id}`}>
-                  <AspectRatio ratio={1}>
-                    <Image src={value.preview} objectFit="cover" />
-                  </AspectRatio>
-                </ChakraLink>
-              </Box>
-            );
-          }
-        })}
-        {isLoading ? (
-          <Flex
-            width="100%"
-            height="100%"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Spinner thickness="4px" speed="0.65s" size="xl" />
-          </Flex>
-        ) : null}
-      </Grid>
+      <Listings
+        fetchedListings={fetchedListings}
+        isLoading={isLoading}
+        sub={sub}
+        setBottom={setBottom}
+      />
     </Container>
   );
 };
