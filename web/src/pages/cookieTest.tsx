f1e29a12a3e61db2689828a535a0c214ced20aca
anselbrandt <ansel@anselbrandt.com>
2020-11-24 03:28:29 -0500

    Test pages working

--- web/src/pages/cookieTest.tsx
+++ web/src/pages/cookieTest.tsx
@@ -1,13 +1,33 @@
 import { Box, Link as ChakraLink } from "@chakra-ui/core";
+import { useEffect, useState } from "react";
 import { Container } from "../components/Container";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import { Navbar } from "../components/Navbar";
+import Cookie from "js-cookie";
 
 interface CookieTestProps {
   defaultColor: string;
 }
 
 const CookieTest: React.FC<CookieTestProps> = ({ defaultColor }) => {
+  const [cookies, setCookies] = useState<string | undefined>();
+  const [sub, setSub] = useState<string | undefined>();
+
+  useEffect(() => {
+    const readCookies = Cookie.getJSON();
+    setCookies(JSON.stringify(readCookies));
+  });
+
+  useEffect(() => {
+    const searchTerm = Cookie.get("searchTerm");
+    if (searchTerm) {
+      Cookie.set("subreddit", searchTerm);
+      setSub(searchTerm);
+    } else {
+      Cookie.remove("subreddit");
+    }
+  }, []);
+
   return (
     <Container minHeight="100vh">
       <DarkModeSwitch defaultColor={defaultColor} />
@@ -15,7 +35,8 @@ const CookieTest: React.FC<CookieTestProps> = ({ defaultColor }) => {
         <ChakraLink href="/cookieTest">cookieTest</ChakraLink>
       </Navbar>
       <Box mt="30vh" maxW="48rem">
-        Cookie Test
+        <Box>{sub ? `from ${sub}` : "no previous results"}</Box>
+        <Box>{cookies}</Box>
       </Box>
     </Container>
   );
--- web/src/pages/test.tsx
+++ web/src/pages/test.tsx
@@ -1,13 +1,45 @@
-import { Box, Link as ChakraLink } from "@chakra-ui/core";
+import { Box, Link as ChakraLink, Input } from "@chakra-ui/core";
+import { useEffect, useState } from "react";
 import { Container } from "../components/Container";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import { Navbar } from "../components/Navbar";
+import Cookie from "js-cookie";
 
 interface TestProps {
   defaultColor: string;
 }
 
 const Test: React.FC<TestProps> = ({ defaultColor }) => {
+  const target = "cabinporn";
+  const [searchTerm, setSearchTerm] = useState<string | undefined>("");
+  const [cookies, setCookies] = useState<string | undefined>();
+  const [prevSearch, setPrevSearch] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (searchTerm === target) {
+      Cookie.set("searchTerm", searchTerm);
+    } else {
+      Cookie.remove("searchTerm");
+    }
+  }, [searchTerm]);
+
+  useEffect(() => {
+    const readCookies = Cookie.getJSON();
+    setCookies(JSON.stringify(readCookies));
+  });
+
+  useEffect(() => {
+    const prevResult = Cookie.get("subreddit");
+    if (prevResult) {
+      setSearchTerm(prevResult);
+    }
+  }, []);
+
+  const handleSearch = (event: any) => {
+    const value = event.currentTarget.value;
+    setSearchTerm(value);
+    setPrevSearch(value);
+  };
   return (
     <Container minHeight="100vh">
       <DarkModeSwitch defaultColor={defaultColor} />
@@ -15,8 +47,25 @@ const Test: React.FC<TestProps> = ({ defaultColor }) => {
         <ChakraLink href="/cookieTest">cookieTest</ChakraLink>
       </Navbar>
       <Box mt="30vh" maxW="48rem">
-        Test Page
+        <Box>
+          {prevSearch ? `prevSearch: ${prevSearch}` : "no previous search"}
+        </Box>
+        <Input
+          type="text"
+          borderRadius="0"
+          list="subreddits"
+          placeholder="search for subreddit"
+          autoComplete="off"
+          autoCorrect="off"
+          autoCapitalize="off"
+          spellCheck="false"
+          onChange={handleSearch}
+          value={searchTerm}
+        />
       </Box>
+      <Box>{searchTerm}</Box>
+      <Box>{searchTerm === target ? "match!" : "does not match"}</Box>
+      <Box>{cookies}</Box>
     </Container>
   );
 };
