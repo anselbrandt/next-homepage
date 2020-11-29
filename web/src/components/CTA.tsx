a6e7abef0a2f37539bdc0e0e74999a1c54bce80b
anselbrandt <ansel@anselbrandt.com>
2020-11-29 17:40:04 -0500

    Style CTA button

--- web/src/components/CTA.tsx
+++ web/src/components/CTA.tsx
@@ -1,4 +1,4 @@
-import { Link as ChakraLink, Button } from "@chakra-ui/core";
+import { Link as ChakraLink, Button, useColorMode } from "@chakra-ui/core";
 
 import { Container } from "./Container";
 
@@ -6,35 +6,44 @@ interface CTAProps {
   defaultColor: string;
 }
 
-export const CTA: React.FC<CTAProps> = ({ defaultColor }) => (
-  <Container
-    flexDirection="row"
-    position="fixed"
-    bottom="0"
-    width="100%"
-    maxWidth="48rem"
-    py={2}
-  >
-    <ChakraLink
-      isExternal
-      href="https://next.chakra-ui.com/"
-      flexGrow={1}
-      mx={2}
+export const CTA: React.FC<CTAProps> = ({ defaultColor }) => {
+  const { colorMode } = useColorMode();
+  const bgColor = { light: "white", dark: "gray.800" };
+  return (
+    <Container
+      flexDirection="row"
+      position="fixed"
+      bottom="0"
+      width="100%"
+      maxWidth="48rem"
+      py={2}
     >
-      <Button width="100%" variant="outline" colorScheme={defaultColor}>
-        chakra-ui
-      </Button>
-    </ChakraLink>
+      <ChakraLink
+        isExternal
+        href="https://next.chakra-ui.com/"
+        flexGrow={1}
+        mx={2}
+      >
+        <Button
+          width="100%"
+          variant="outline"
+          colorScheme={defaultColor}
+          backgroundColor={bgColor[colorMode]}
+        >
+          chakra-ui
+        </Button>
+      </ChakraLink>
 
-    <ChakraLink
-      isExternal
-      href="https://github.com/anselbrandt/next-express/"
-      flexGrow={3}
-      mx={2}
-    >
-      <Button width="100%" variant="solid" colorScheme={defaultColor}>
-        View Repo
-      </Button>
-    </ChakraLink>
-  </Container>
-);
+      <ChakraLink
+        isExternal
+        href="https://github.com/anselbrandt/next-express/"
+        flexGrow={3}
+        mx={2}
+      >
+        <Button width="100%" variant="solid" colorScheme={defaultColor}>
+          View Repo
+        </Button>
+      </ChakraLink>
+    </Container>
+  );
+};
