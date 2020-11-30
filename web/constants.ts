040d20a1d792205c88fc36d5394077001918f21b
anselbrandt <ansel@anselbrandt.com>
2020-11-30 16:41:31 -0500

    Update CTA

--- web/constants.ts
+++ web/constants.ts
@@ -15,6 +15,10 @@ export const MAPBOX_ACCESS_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
 export const COOKIE_NAME = process.env.NEXT_PUBLIC_COOKIE_NAME || "qid";
 
+export const EMAIL = "ansel@anselbrandt.com";
+
+export const CODE_REPO = "https://github.com/anselbrandt/next-homepage";
+
 export const pages = [
   { path: "/error", title: "404" },
   { path: "/about", title: "About" },
--- web/src/components/CTA.tsx
+++ web/src/components/CTA.tsx
@@ -1,4 +1,6 @@
 import { Link as ChakraLink, Button, useColorMode } from "@chakra-ui/core";
+import NextLink from "next/link";
+import { EMAIL, CODE_REPO } from "../../constants";
 
 import { Container } from "./Container";
 
@@ -19,32 +21,31 @@ export const CTA: React.FC<CTAProps> = ({ defaultColor }) => {
       pb={2}
       backgroundColor={bgColor[colorMode]}
     >
-      <ChakraLink
-        isExternal
-        href="https://next.chakra-ui.com/"
-        flexGrow={1}
-        mx={2}
-      >
+      <NextLink href={`mailto:${EMAIL}`} passHref>
         <Button
+          as="a"
           width="100%"
           variant="outline"
           colorScheme={defaultColor}
-          backgroundColor={bgColor[colorMode]}
+          flexGrow={1}
+          mx={2}
         >
-          chakra-ui
+          contact
         </Button>
-      </ChakraLink>
+      </NextLink>
 
-      <ChakraLink
-        isExternal
-        href="https://github.com/anselbrandt/next-express/"
-        flexGrow={3}
-        mx={2}
-      >
-        <Button width="100%" variant="solid" colorScheme={defaultColor}>
-          View Repo
+      <NextLink href={CODE_REPO} passHref>
+        <Button
+          as="a"
+          width="100%"
+          variant="solid"
+          colorScheme={defaultColor}
+          flexGrow={3}
+          mx={2}
+        >
+          view code
         </Button>
-      </ChakraLink>
+      </NextLink>
     </Container>
   );
 };
