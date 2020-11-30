ff6d601f27fa35e3dc86325db1eeb33ec383ecd7
anselbrandt <ansel@anselbrandt.com>
2020-11-30 16:41:56 -0500

    Update Hero

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -2,12 +2,12 @@ import { Flex, Heading } from "@chakra-ui/core";
 
 export const Hero = ({ title }: { title: string }) => (
   <Flex justifyContent="center" height="100vh">
-    <Heading fontSize="6vw" mt="20vh">
+    <Heading fontSize="10vw" mt="20vh">
       {title}
     </Heading>
   </Flex>
 );
 
 Hero.defaultProps = {
-  title: "next-express-graphql.tsx",
+  title: "ansel brandt",
 };
