4e562ee0c7de89ba236cf2d5255ebd26064c8f03
anselbrandt <ansel@anselbrandt.com>
2020-12-08 04:43:27 -0500

    Make MapLegend collapsible

--- web/src/components/MapLegend.tsx
+++ web/src/components/MapLegend.tsx
@@ -1,6 +1,17 @@
 import React from "react";
-import { Box, useColorMode, IconButton, Grid, GridItem } from "@chakra-ui/core";
-import { EditIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  useColorMode,
+  IconButton,
+  Grid,
+  GridItem,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from "@chakra-ui/core";
+import { EditIcon, ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import { colorsArr } from "../utils/mapUtils";
 
 interface MapLegendProps {
@@ -20,7 +31,7 @@ export const MapLegend: React.FC<MapLegendProps> = ({
   const bgColor = { light: "white", dark: "gray.800" };
   const color = { light: "black", dark: "white" };
 
-  const startRow = [4, 6, 8, 10, 12, 14, 16, 18];
+  const startRow = [2, 4, 6, 8, 10, 12, 14, 16];
 
   return (
     <Box
@@ -34,72 +45,90 @@ export const MapLegend: React.FC<MapLegendProps> = ({
       borderWidth="2px"
       rounded="lg"
     >
-      <Grid
-        templateRows="repeat(19, 1fr)"
-        templateColumns="repeat(6, 1fr)"
-        gap={1}
-      >
-        <GridItem colSpan={6} rowSpan={2} textAlign="center" fontSize="md">
-          Assessments
-        </GridItem>
-        {colorsArr.map((color, index) => (
-          <GridItem
-            key={index}
-            colStart={1}
-            rowSpan={2}
-            colSpan={1}
-            bgColor={color}
-          />
-        ))}
-        <GridItem colStart={2} rowStart={3} colSpan={5} />
-        {bins
-          .filter((value) => value !== 0)
-          .map((value, index) => (
-            <GridItem
-              key={index}
-              colStart={2}
-              rowStart={startRow[index]}
-              rowSpan={2}
-              colSpan={3}
-              fontSize="sm"
-            >
-              ${(+(value / 1000).toFixed(0)).toLocaleString()}k
-            </GridItem>
-          ))}
-        {range.reverse().map((value, index) => (
-          <GridItem
-            key={index}
-            colStart={5}
-            rowStart={startRow[index]}
-            rowSpan={2}
-            colSpan={2}
-            fontSize="sm"
-          >
-            +{value * 100}%
-          </GridItem>
-        ))}
-        <GridItem colStart={5} rowStart={10} rowSpan={2} colSpan={2}>
-          <IconButton
-            variant="link"
-            colorScheme={defaultColor}
-            aria-label="Edit price"
-            icon={<EditIcon />}
-            onClick={handleOpen}
-          />
-        </GridItem>
-        {range.sort().map((value, index) => (
-          <GridItem
-            key={index}
-            colStart={5}
-            rowStart={startRow[index + 4]}
-            rowSpan={2}
-            colSpan={2}
-            fontSize="sm"
-          >
-            -{value * 100}%
-          </GridItem>
-        ))}
-      </Grid>
+      <Accordion defaultIndex={[0]} allowMultiple>
+        <AccordionItem>
+          {({ isExpanded }) => (
+            <>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  Legend
+                </Box>
+                {isExpanded ? (
+                  <ChevronDownIcon fontSize="20px" />
+                ) : (
+                  <ChevronUpIcon fontSize="20px" />
+                )}
+              </AccordionButton>
+              <AccordionPanel pb={4}>
+                <Grid
+                  templateRows="repeat(17, 1fr)"
+                  templateColumns="repeat(6, 1fr)"
+                  gap={1}
+                >
+                  {colorsArr.map((color, index) => (
+                    <GridItem
+                      key={index}
+                      colStart={1}
+                      rowSpan={2}
+                      colSpan={1}
+                      bgColor={color}
+                    />
+                  ))}
+                  <GridItem colStart={2} rowStart={1} colSpan={5} />
+                  {bins
+                    .filter((value) => value !== 0)
+                    .map((value, index) => (
+                      <GridItem
+                        key={index}
+                        colStart={2}
+                        rowStart={startRow[index]}
+                        rowSpan={2}
+                        colSpan={3}
+                        fontSize="sm"
+                      >
+                        ${(+(value / 1000).toFixed(0)).toLocaleString()}k
+                      </GridItem>
+                    ))}
+                  {range.reverse().map((value, index) => (
+                    <GridItem
+                      key={index}
+                      colStart={5}
+                      rowStart={startRow[index]}
+                      rowSpan={2}
+                      colSpan={2}
+                      fontSize="sm"
+                    >
+                      +{value * 100}%
+                    </GridItem>
+                  ))}
+                  <GridItem colStart={5} rowStart={8} rowSpan={2} colSpan={2}>
+                    <IconButton
+                      variant="link"
+                      colorScheme={defaultColor}
+                      aria-label="Edit price"
+                      icon={<EditIcon />}
+                      onClick={handleOpen}
+                      fontSize="25px"
+                    />
+                  </GridItem>
+                  {range.sort().map((value, index) => (
+                    <GridItem
+                      key={index}
+                      colStart={5}
+                      rowStart={startRow[index + 4]}
+                      rowSpan={2}
+                      colSpan={2}
+                      fontSize="sm"
+                    >
+                      -{value * 100}%
+                    </GridItem>
+                  ))}
+                </Grid>
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
+      </Accordion>
     </Box>
   );
 };
