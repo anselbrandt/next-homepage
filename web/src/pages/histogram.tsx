b73f07f9f50f5346e7bd951da4c3cbe8010a1070
anselbrandt <ansel@anselbrandt.com>
2020-12-13 10:29:10 -0500

    Update histogram test page

--- web/src/pages/histogram.tsx
+++ web/src/pages/histogram.tsx
@@ -58,6 +58,7 @@ const Test: React.FC<TestProps> = ({ defaultColor }) => {
           fillColor={fillColor[colorMode]}
           initialValue={initialValue}
           handleUpdatePrice={handleUpdatePrice}
+          handleUpdateTarget={handleUpdatePrice}
         />
         <Box mt={30}>{price ? price : getValue(initialValue)}</Box>
       </Flex>
